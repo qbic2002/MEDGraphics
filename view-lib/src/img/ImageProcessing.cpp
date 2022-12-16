e9a823c1989b455bffe150a038accdc74212c7c2
Danilaa56 <danila.aronov@gmail.com>
2022-12-16 12:29:48 +0300

    fix: 4 scale method

--- view-lib/src/img/ImageProcessing.cpp
+++ view-lib/src/img/ImageProcessing.cpp
@@ -134,7 +134,7 @@ namespace img::scale {
             return ((12 - 9 * b - 6 * c) * x * x * x + (-18 + 12 * b + 6 * c) * x * x + (6 - 2 * b)) / 6;
         } else if (x < 2) {
             return ((-b - 6 * c) * x * x * x + (6 * b + 30 * c) * x * x
-                    + (-12 * b - 48 * c) * x + (8 * b + 24 * c));
+                    + (-12 * b - 48 * c) * x + (8 * b + 24 * c)) / 6;
         } else {
             return 0;
         }
